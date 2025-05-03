c485832af61f5657e06538a8d85f111b4787d5d7
Leorev01 <revrennaleo@gmail.com>
2025-05-03 20:46:48 +0100

    Changes hero section image

--- React_Portfolio/src/components/Hero.jsx
+++ React_Portfolio/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from '../constants'
-import profilePic from '../assets/Leo_Profile.jpg'
+import profilePic from '../assets/Leo_Profile2.jpg'
 import {motion} from 'framer-motion'
 
 
