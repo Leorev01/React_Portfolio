3f87ef35de0d2edaecfcb969076c014bffc304a8
Leonardo Revrenna <140798910+Leorev01@users.noreply.github.com>
2026-06-03 20:44:27 +0100

    Change email link to use mailto for contact email

--- React_Portfolio/src/components/Contact.jsx
+++ React_Portfolio/src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
             <motion.p whileInView={{opacity: 1, x:0}}
             initial={{opacity: 0, x:100}}
             transition={{duration:1}} className="my-4">{CONTACT.phoneNo}</motion.p>
-            <a href='#' className="border-b">{CONTACT.email}</a>
+            <a href='mailto:revrennaleo@gmail.com' className="border-b">{CONTACT.email}</a>
         </div>
     </div>
   )
