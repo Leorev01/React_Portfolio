28500b20bc221d5fd27d9b25ac81f6816150fac6
Leonardo Revrenna <140798910+Leorev01@users.noreply.github.com>
2026-06-08 10:36:42 +0100

    Implement social media click tracking in Navbar
    Added clickHandler function to track social media interactions.

--- React_Portfolio/src/components/Navbar.jsx
+++ React_Portfolio/src/components/Navbar.jsx
@@ -2,15 +2,22 @@ import logo from '../assets/LeoLogo.png'
 import { FaGithub, FaLinkedin, FaYoutube } from 'react-icons/fa'
 
 const Navbar = () => {
+
+  function clickHandler(socialType){
+    window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+      'socialView': socialType
+    })
+  }
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
         <div className='flex flex-shrink-0 items-center'>
             <img className='mx-2 w-20' src={logo} alt="logo" />
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-            <a href='https://www.youtube.com/@CrappyCoder2001' target='_blank' className='social' ><FaYoutube /></a>
-            <a href='https://github.com/Leorev01' target='_blank' className='social' ><FaGithub /></a>
-            <a href='https://www.linkedin.com/in/leorev01' target='_blank' className='social'><FaLinkedin /></a>
+            <a href='https://www.youtube.com/@CrappyCoder2001' target='_blank' className='social' onClick={() => clickHandler('Youtube')}><FaYoutube /></a>
+            <a href='https://github.com/Leorev01' target='_blank' className='social' onClick={() => clickHandler('GitHub')}><FaGithub /></a>
+            <a href='https://www.linkedin.com/in/leorev01' target='_blank' className='social' onClick={() => clickHandler('LinkedIn')}><FaLinkedin /></a>
         </div>
     </nav>
   )
